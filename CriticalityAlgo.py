8010832adb72a6c5cff3b935cb9491371034fb1d
damithperera10 <damithperera10@gmail.com>
2018-09-09 22:16:53 +0530

    wordcloud generate fucntion added

--- CriticalityAlgo.py
+++ CriticalityAlgo.py
@@ -2,6 +2,8 @@ import nltk
 import csv
 from nltk.corpus import stopwords
 import pickle
+import matplotlib.pyplot as plt
+from wordcloud import WordCloud, STOPWORDS
 
 data = []
 with open('dataSet.csv') as csvDataFile:
@@ -33,6 +35,27 @@ for index, row in data:
     words_without_stopwords = [word for word in words_cleaned if not word in stopwords_set]
     tweets.append((words_without_stopwords, row))
 
+
+# draw worldCloud
+def wordcloud_draw(data, color='black'):
+    words = ' '.join(data)
+    cleaned_word = " ".join([word for word in words.split()
+                             if 'http' not in word
+                             and not word.startswith('@')
+                             and not word.startswith('#')
+                             and word != 'RT'
+                             ])
+    wordcloud = WordCloud(stopwords=STOPWORDS,
+                          background_color=color,
+                          width=2500,
+                          height=2000
+                          ).generate(cleaned_word)
+    plt.figure(1, figsize=(13, 13))
+    plt.imshow(wordcloud)
+    plt.axis('off')
+    plt.show()
+
+
 # Extracting word features
 def get_words_in_tweets(tweets):
     all = []
@@ -50,6 +73,8 @@ def get_word_features(wordlist):
 w_features = get_word_features(get_words_in_tweets(tweets))
 
 
+# wordcloud_draw(w_features, 'black')
+
 def extract_features(document):
     document_words = set(document)
     features = {}
@@ -57,6 +82,15 @@ def extract_features(document):
         features['contains(%s)' % word] = (word in document_words)
     return features
 
+
+# wordcloud for high
+# wordcloud_draw(train_high, 'red')
+
+# wordcloud for medium
+# wordcloud_draw(train_medium, 'black')
+
+# wordcloud for low
+#g wordcloud_draw(train_low, 'green')
 
 # Training the Naive Bayes classifier
 training_set = nltk.classify.apply_features(extract_features, tweets)
