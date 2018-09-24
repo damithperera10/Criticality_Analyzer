08573c32cef64f44e2b224f24329c1ab0ba96032
damithperera10 <damithperera10@gmail.com>
2018-09-25 00:58:15 +0530

    criticality analising api imlementation and end point imlementaion

--- CriticaliryController.py
+++ CriticaliryController.py
@@ -8,6 +8,12 @@ import json
 import decimal
 from boto3.dynamodb.conditions import Key, Attr
 from botocore.exceptions import ClientError
+from flask import Flask, request
+from flask_restful import Resource, Api
+from flask import jsonify
+
+app = Flask(__name__)
+api = Api(app)
 
 loadModal = pickle.load(open('CriticalityAnalysisModal.sav', 'rb'))
 stopwords_set = set(stopwords.words("english"))
@@ -24,50 +30,56 @@ class DecimalEncoder(json.JSONEncoder):
 
 
 dynamodb = boto3.resource('dynamodb',
-                          aws_access_key_id="AKIAJT3NTIISNIZ54VUQ",
-                          aws_secret_access_key="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",
+                          aws_access_key_id="AKIAJ3G6N4MARQVO6JJA",
+                          aws_secret_access_key="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",
                           region_name="us-east-1",
                           endpoint_url="https://dynamodb.us-east-1.amazonaws.com"
                           )
 
 table = dynamodb.Table('social_media_post')
-response = []
-
-try:
-    response = table.scan(
-        FilterExpression=Attr('accuracy').eq('high') | Attr('accuracy').eq('medium')
-    )
-except ClientError as e:
-    print(e.response['Error']['Message'])
-
-items = response['Items']
-for i in items:
-    data = i['post']
-    newData = data
-    data = data.lower()
-    wordsSet = data.split()
-    CleanData = [word for word in wordsSet
-                 if 'http' not in word
-                 and not word.startswith('@')
-                 and not word.startswith('#')
-                 and word != 'RT']
-    words_without_stopwords = [word for word in CleanData if not word in stopwords_set]
-    result = loadModal.classify(extract_features(words_without_stopwords))
-    print(i['id'] + ' result is: ' + result)
-    table.update_item(
-        Key={
-            'id': i['id'],
-        },
-        UpdateExpression="set criticality = :val",
-        ExpressionAttributeValues={
-            ':val': result
-        },
-        ReturnValues="UPDATED_NEW"
-    )
-    newDataFromResult = [{newData, result}]
-    newDataFile = open('newData.csv', 'a')
-    with newDataFile:
-        writer = csv.writer(newDataFile)
-        writer.writerows(newDataFromResult)
 
 
+@app.route('/criticality/<string:post_id>', methods=['GET'])
+def get_post(post_id):
+    try:
+        response = table.scan(
+            FilterExpression=Attr('id').eq(post_id)
+        )
+        item = response['Items']
+        data = item[0]['post']
+        new_data = data
+        data = data.lower()
+        words_set = data.split()
+        clean_data = [word for word in words_set
+                     if 'http' not in word
+                     and not word.startswith('@')
+                     and not word.startswith('#')
+                     and word != 'RT']
+        words_without_stopwords = [word for word in clean_data if not word in stopwords_set]
+        result = loadModal.classify(extract_features(words_without_stopwords))
+
+        table.update_item(
+            Key={
+                'id': item[0]['id'],
+            },
+            UpdateExpression="set criticality = :val",
+            ExpressionAttributeValues={
+                ':val': result
+            },
+            ReturnValues="UPDATED_NEW"
+        )
+        print(item[0]['id'] + ' result is: ' + result + ' and data base update successful.')
+        new_data_from_result = [{new_data, result}]
+        new_data_file = open('newData.csv', 'a')
+        with new_data_file:
+            writer = csv.writer(new_data_file)
+            writer.writerows(new_data_from_result)
+
+        return jsonify(item[0]['id'])
+    except ClientError as e:
+        print(e.response['Error']['Message'])
+
+
+if __name__ == '__main__':
+    print("Server is running on port 5002")
+    app.run(port='5002')
